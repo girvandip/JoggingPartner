aceef584a98d42a35dcea5d931d430332a953737
aldrichvalentino <aldrich.vh97@gmail.com>
2018-02-15 23:01:59 +0700

    added recycler view for data

---
+++ app/src/main/java/com/example/batere3a/joggingpartner/OrderListAdapter.java
@@ -0,0 +1,52 @@
+package com.example.batere3a.joggingpartner;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Aldrich on 2/15/2018.
+ */
+
+public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.OrderViewHolder> {
+    private final LinkedList<String> mWordList;
+
+    public OrderListAdapter(LinkedList<String> orderList) {
+        this.mWordList = orderList;
+    }
+
+    @Override
+    public OrderListAdapter.OrderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.orderlist_item, parent, false);
+        return new OrderViewHolder(view, this);
+    }
+
+    @Override
+    public void onBindViewHolder(OrderListAdapter.OrderViewHolder holder, int position) {
+        // TODO: check the bind variables to output order data
+        String current = mWordList.get(position);
+        holder.orderItemView.setText(current);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+    class OrderViewHolder extends RecyclerView.ViewHolder {
+        public final TextView orderItemView;
+        final OrderListAdapter mAdapter;
+
+        public OrderViewHolder(View itemView, OrderListAdapter adapter) {
+            super(itemView);
+            orderItemView = (TextView) itemView.findViewById(R.id.order);
+            this.mAdapter = adapter;
+        }
+    }
+}
--- app/src/main/java/com/example/batere3a/joggingpartner/Orders.java
+++ app/src/main/java/com/example/batere3a/joggingpartner/Orders.java
@@ -3,27 +3,122 @@ package com.example.batere3a.joggingpartner;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.LinkedList;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Orders extends Fragment {
+    protected LinkedList<String> mWordList;
+    protected RecyclerView recyclerView;
+    protected OrderListAdapter orderListAdapter;
+    protected RecyclerView.LayoutManager layoutManager;
+
+    private enum LayoutManagerType {
+        GRID_LAYOUT_MANAGER,
+        LINEAR_LAYOUT_MANAGER
+    }
+
+    protected LayoutManagerType mCurrentLayoutManagerType;
+
+    // Firebase database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Users");
 
 
     public Orders() {
-        // Required empty public constructor
+        // Pull data from database
+        mWordList = new LinkedList<>();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        // initiate linked list
+        for(int i = 0; i < 10; i++){
+            mWordList.addLast("ini i = " + i);
+        }
+
+        // Firebase write test
+        myRef.setValue("Hello, World!");
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_orders, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview);
+
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        // set layout manager
+        mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        orderListAdapter = new OrderListAdapter(mWordList);
+        recyclerView.setAdapter(orderListAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false);
+        return rootView;
+    }
+
+    public void setRecyclerViewLayoutManager(LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (recyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) recyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        switch (layoutManagerType) {
+            case GRID_LAYOUT_MANAGER:
+                layoutManager = new GridLayoutManager(getActivity(), 2);
+                mCurrentLayoutManagerType = LayoutManagerType.GRID_LAYOUT_MANAGER;
+                break;
+            case LINEAR_LAYOUT_MANAGER:
+                layoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+                break;
+            default:
+                layoutManager = new LinearLayoutManager(getActivity());
+                mCurrentLayoutManagerType = LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+        }
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.scrollToPosition(scrollPosition);
     }
 
 }
