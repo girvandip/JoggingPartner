b0ee6e41e61758199b83be5d8afc10a61224c139
aldrichvalentino <aldrich.vh97@gmail.com>
2018-02-22 09:21:32 +0700

    added proximity sensor for calling

--- app/src/main/java/com/example/batere3a/joggingpartner/order/OrderDetails.java
+++ app/src/main/java/com/example/batere3a/joggingpartner/order/OrderDetails.java
@@ -1,6 +1,16 @@
 package com.example.batere3a.joggingpartner.order;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +24,12 @@ import com.example.batere3a.joggingpartner.R;
 
 import org.json.JSONObject;
 
-public class OrderDetails extends AppCompatActivity {
+public class OrderDetails extends AppCompatActivity implements SensorEventListener {
     private String dataId;
     private String data;
-
+    private SensorManager sensorManager;
+    private Sensor proximitySensor;
+    private String orderStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,7 @@ public class OrderDetails extends AppCompatActivity {
             // bind the data
             TextView status = findViewById(R.id.order_status);
             status.setText(jsonData.getString("status"));
+            this.orderStatus = jsonData.getString("status");
 
             if (jsonData.getString("status").equals("Progress")) {
                 View separator = (View) findViewById(R.id.phone_separator);
@@ -62,6 +75,56 @@ public class OrderDetails extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Get sensor
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, proximitySensor,
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float distance = sensorEvent.values[0];
+        if (distance <= 1.0 && orderStatus.equals("Progress")) {
+            // get the number
+            TextView phoneView = findViewById(R.id.partner_phone);
+            String phone = "tel:" + phoneView.getText();
+
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse(phone));
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_CALL_PHONE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            } else {
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
